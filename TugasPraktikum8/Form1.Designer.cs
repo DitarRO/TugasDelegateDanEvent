9c71638810899d52e6be91f8aeb2c6eb116bb919
DitarRO <ditarrozano2013@gmail.com>
2022-12-05 20:50:36 +0700

    Add project files.

---
+++ TugasPraktikum8/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TugasPraktikum8
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.listBox1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(411, 270);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "[Hasil]";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(18, 295);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(399, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Hitung";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(6, 19);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(399, 238);
+			this.listBox1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(448, 334);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Hasil Perhtungan";
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ TugasPraktikum8/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasPraktikum8
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Calculator calculator = new Calculator();
+			calculator.handler += tampilan;
+			calculator.ShowDialog();
+		}
+
+		private void tampilan(int nilaiA, int nilaiB, int hasil, string simbol, string operasihitung)
+		{
+			listBox1.Items.Add(string.Format("Hasil {0} {1} {2} {3} = {4}",operasihitung,  nilaiA, simbol, nilaiB, hasil));
+		}
+	}
+}
